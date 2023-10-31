1c5890e666901d6f7df9342412142e6731894dcd
YukaHirai <133447397+yusoraka@users.noreply.github.com>
2023-10-31 13:33:30 +0000

    add homes/top

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+
+ namespace :admin do
+    root to: 'homes#top'
+    resources :comment
+    resources :user
+    resources :recipe
+    resources :homes
+  end
+
+  scope module: :public do
+    root to: 'homes#top'
+
+  end
+
+devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
